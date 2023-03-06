4b2f5f793b6181a2f4bd46c5f6b59a303328703a
Katia <vai.katia03@mail.ru>
2023-03-06 15:25:24 +0300

    meet06.03

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Chat from './pages/Chat/Chat';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Chat/Chat.js
@@ -0,0 +1,291 @@
+import { useEffect, useRef, useState } from "react";
+// import logo from '../../assets/inno.png'
+// import { Socket, io } from "engine.io-client";
+import { w3cwebsocket } from "websocket";
+import "./chat.css";
+
+const config = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
+//   const SOCKET_SERVER_URL = "http://localhost:8080";
+
+function Chat() {
+  let [perName, setName] = useState("");
+  // let localVideo = useRef();
+  // let remoteVideo = useRef();
+  let localVideo = useRef();
+  let remoteVideo = useRef();
+  let otherUser;
+  let [myName, setMyName] = useState("");
+  let localStream;
+  let remoteStream;
+  let peerConnection
+  let callSocket=new WebSocket("ws://192.168.25.192:8000/room/name");;
+  let remoteRtcMessage;
+  let iceCandidatesFromCaller = [];
+  let [callProg, setCallProgress] = useState(false);
+
+  const call = () => {
+    let userToCall = perName;
+    otherUser = userToCall;
+    console.log(myName)
+
+    beReady().then((bool) => {
+      processCall(userToCall);
+    });
+  };
+
+  const answer = () => {
+    beReady().then((bool) => {
+      processAccept();
+    });
+  };
+
+  const connectSocket = () => {
+    callSocket = new WebSocket("ws://192.168.25.192:8000/room/name");
+    callSocket.onopen = (e) => {
+      callSocket.send(
+        JSON.stringify({
+          type: "login",
+          data: {
+            name: myName,
+          },
+        })
+      );
+    };
+
+    callSocket.onmessage = (e) => {
+      let response = JSON.parse(e.data);
+      let type = response.type;
+
+      if (type == "connection") {
+        console.log(response.data.message);
+      }
+      if (type == "call_received") {
+        onNewCall(response.data);
+      }
+      if (type == "call_answered") {
+        onCallAnswered(response.data);
+      }
+      if (type == "ICEcandidate") {
+        onICECandidate(response.data);
+      }
+    };
+  };
+
+  const onNewCall=(data)=>{
+    otherUser=data.caller
+    remoteRtcMessage=data.rtcMessage
+  }
+
+  const onCallAnswered=(data)=>{
+    remoteRtcMessage=data.rtcMessage
+    peerConnection.setRemoteDescription(new RTCSessionDescription(remoteRtcMessage))
+    callProgress()
+  }
+
+  const onICECandidate=(data)=>{
+    let message=data.rtcMessage
+    let candidate=new RTCIceCandidate({
+      sdpMLineIndex:message.label,
+      candidate:message.candidate
+    })
+    if(peerConnection){
+      peerConnection.addIceCandidate(candidate)
+    }else{
+      iceCandidatesFromCaller.push(candidate)
+    }
+  }
+
+  const processAccept = () => {
+    peerConnection.setRemoteDescription(
+      new RTCSessionDescription(remoteRtcMessage)
+    );
+    peerConnection.createAnswer((session) => {
+      peerConnection.setLocalDescription(session);
+      if (iceCandidatesFromCaller.length > 0) {
+        for (let i = 0; i < iceCandidatesFromCaller.length; i++) {
+          let candidate = iceCandidatesFromCaller[i];
+          try {
+            peerConnection
+              .addIceCandidate(candidate)
+              .then((done) => {
+                console.log(done);
+              })
+              .catch((err) => console.log(err));
+          } catch (err) {
+            console.log(err);
+          }
+        }
+        iceCandidatesFromCaller = [];
+      }
+      answerCall({
+        caller: otherUser,
+        rtcMessage: session,
+      });
+    });
+  };
+
+  const answerCall = (data) => {
+    callSocket.send(
+      JSON.stringify({
+        type: "answer_call",
+        data,
+      })
+    );
+    callProgress();
+  };
+
+  const callProgress = () => {
+    setCallProgress(true);
+  };
+
+  const processCall = (username) => {
+    peerConnection.createOffer(
+      (session) => {
+        peerConnection.setLocalDescription(session);
+        sendCall({
+          name: username,
+          rtcMessage: session,
+        });
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
+
+  const sendCall = (data) => {
+    console.log("sendCall");
+    callSocket.send(
+      JSON.stringify({
+        type: "call",
+        data,
+      })
+    );
+    //окошко вызова
+  };
+
+  const beReady = () => {
+       return navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      }).then((stream)=>{
+      localStream = stream;
+      localVideo.current.srcObject = stream;
+      return createConnectionAndAddStream();
+    }).catch( (err)=>{
+      console.log(err);
+    })
+  };
+
+  const createConnectionAndAddStream = () => {
+    createPeerConnection();
+    peerConnection.addStream(localStream);
+    return true;
+  };
+
+  const createPeerConnection = () => {
+    try {
+      peerConnection = new RTCPeerConnection(config);
+      peerConnection.onicecandidate = handleIceCandidate;
+      peerConnection.onaddstream = handleRemoteStreamAdded;
+      peerConnection.onremovestream = handleRemoteStreamRemoved;
+      console.log("create rtc peer connection");
+      return;
+    } catch (e) {
+      console.log("faild peer ", e.message);
+      return;
+    }
+  };
+
+  const handleRemoteStreamAdded = (event) => {
+    console.log("Remote stream added.");
+    remoteStream = event.stream;
+    remoteVideo.srcObject = remoteStream;
+  };
+  const handleRemoteStreamRemoved = (event) => {
+    console.log("Remote stream removed. Event: ", event);
+    remoteVideo.srcObject = null;
+    localVideo.srcObject = null;
+
+    window.onbeforeunload=()=>{
+      if(callProg){
+        stop()
+      }
+    }
+  };
+
+  const stop=()=>{
+    localStream.getTracks().forEach(track => {
+      track.stop()
+    });
+    setCallProgress(false)
+    peerConnection.close()
+    peerConnection=null
+    otherUser=null
+  }
+
+  const handleIceCandidate = (e) => {
+    if (e.candidate) {
+      console.log("local ice candidate");
+      sendIceCandidate({
+        user: otherUser,
+        rtcMessage: {
+          label: e.candidate.sdpMLineIndex,
+          id: e.candidate.sdpMid,
+          candidate: e.candidate.candidate,
+        },
+      });
+    } else {
+      console.log("end candidate");
+    }
+  };
+
+  const sendIceCandidate = (data) => {
+    console.log("send ice candadati",callSocket);
+    callSocket.send(
+      JSON.stringify({
+        type: "ICEcandidate",
+        data,
+      })
+    );
+  };
+
+  const login =()=>{
+    connectSocket()
+  }
+
+  return (
+    <div>
+      <label>who</label>
+      <input
+        onChange={(e) => {
+          setMyName(e.target.value);
+        }}
+        value={myName}
+      />
+      <div onClick={login}>login</div>
+
+      <label>whom</label>
+      <input
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        value={perName}
+      />
+      <div onClick={call}>call</div>
+
+      <div onClick={answer}>answer</div>
+
+      <video muted autoPlay ref={localVideo}></video>
+      <video autoPlay ref={remoteVideo}></video>
+    </div>
+  );
+}
+
+export default Chat;
