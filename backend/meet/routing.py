b47370ef131314613066a148d20a0f90efcc9cc2
vladbyk <byk_vlad@inbox.ru>
2023-03-10 12:03:16 +0300

    csrf trusted origins

--- backend/meet/routing.py
+++ backend/meet/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import ConferenceConsumer
 
 websocket_urlpatterns = [
-    path('room/<str:room>', ConferenceConsumer.as_asgi()),
+    path('room/', ConferenceConsumer.as_asgi()),
 ]
