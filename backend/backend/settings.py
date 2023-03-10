ed874511955bf2beaf9eff24770abceb7debbfaf
vladbyk <byk_vlad@inbox.ru>
2023-03-10 12:11:39 +0300

    csrf trusted origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,7 +188,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('webrtc-redis-layer', '6379')],
+            "hosts": [("0.0.0.0", 6379)],
         },
     },
 }
