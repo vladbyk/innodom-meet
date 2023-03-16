023f47a7b7b8c990943453d31dcf44f4c810fda7
vladbyk <byk_vlad@inbox.ru>
2023-03-16 16:56:10 +0300

    csrf trusted origins

--- backend/meet/consumers.py
+++ backend/meet/consumers.py
@@ -1,14 +1,13 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class VideoChatConsumer(AsyncWebsocketConsumer):
+class VideoConferenceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Получение идентификатора комнаты из URL-адреса
-        self.room_id = self.scope['url_route']['kwargs']['room_id']
-        self.room_group_name = 'video_chat_%s' % self.room_id
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'videoconference_%s' % self.room_name
 
-        # Присоединение к группе WebSocket
+        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -17,24 +16,46 @@ class VideoChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Удаление из группы WebSocket
+        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
+    # Receive message from WebSocket
     async def receive(self, text_data):
-        # Получение сообщения от клиента и отправка его в группу WebSocket
         message = json.loads(text_data)
+        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message
+                'type': message['type'],
+                'message': message['message']
             }
         )
 
-    async def chat_message(self, event):
-        # Отправка сообщения из группы WebSocket клиентам
-        message = event['message']
-        await self.send(text_data=json.dumps(message))
+    # Receive message from room group
+    async def video_offer(self, event):
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'video_offer',
+            'message': event['message']
+        }))
+
+    async def video_answer(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'video_answer',
+            'message': event['message']
+        }))
+
+    async def new_ice_candidate(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'new_ice_candidate',
+            'message': event['message']
+        }))
+
+    async def peer_left(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'peer_left',
+            'message': event['message']
+        }))
