53ba82f0e9b2af0bc4f2c6333c073caa739e0389
Katia <vai.katia03@mail.ru>
2023-03-16 17:00:51 +0300

    conf16

--- frontend/src/pages/Chat/Chat.js
+++ frontend/src/pages/Chat/Chat.js
@@ -1,149 +1,160 @@
-// Импортирование необходимых модулей
-import React, { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
+import React, { useState, useEffect, useRef } from "react";
 
-// Создание компонента приложения
-function Chat() {
-  const [peerConnections, setPeerConnections] = useState([]); // Список установленных соединений с другими пользователями
-  const [socket, setSocket] = useState(null); // WebSocket для обмена сообщениями с сервером
-  const [roomId, setRoomId] = useState(''); // Идентификатор комнаты, в которой находится пользователь
-  const localVideoRef = useRef(null); // Ссылка на локальный видеоэлемент
-  const remoteVideosRef = useRef([]); // Ссылки на удаленные видеоэлементы
-  
-  // Установка соединения с сервером WebSocket
+const WebRTCVideoConference = () => {
+  const [localStream, setLocalStream] = useState(null);
+  const [remoteStreams, setRemoteStreams] = useState([]);
+  const [peerConnections, setPeerConnections] = useState([]);
+  const [roomId, setRoomId] = useState("");
+
+  const localVideoRef = useRef(null);
+  const remoteVideosRef = useRef([]);
+
   useEffect(() => {
-    const newSocket = io(`wss://rims.by/ws/video_chat/${roomId}/`);
-    setSocket(newSocket);
-    
-    return () => {
-      newSocket.disconnect();
-    };
-  }, []);
-  const newRemoteVideoRef = useRef(null);
-  
-  // Установка локального потока медиа
-  useEffect(() => {
-    const getLocalMediaStream = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    const getLocalStream = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      setLocalStream(stream);
       localVideoRef.current.srcObject = stream;
-      localVideoRef.current.muted = true;
-      
-       // Создание RTCPeerConnection и добавление локального потока медиа
-  const peerConnection = new RTCPeerConnection();
-  peerConnection.addStream(stream);
-  
-  // Обработка события icecandidate и отправка кандидата на сервер
-  peerConnection.onicecandidate = (event) => {
-    if (event.candidate) {
-      socket.emit('candidate', { roomId, candidate: event.candidate });
-    }
-  };
-  
-  // Обработка события addstream и добавление удаленного потока медиа
-  peerConnection.onaddstream = (event) => {
-    const remoteVideoRef = remoteVideosRef.current.find(ref => ref.id === event.stream.id);
-    if (remoteVideoRef) {
-      remoteVideoRef.srcObject = event.stream;
-    } else {
-      remoteVideosRef.current = [...remoteVideosRef.current, newRemoteVideoRef];
-      setPeerConnections(prevState => [...prevState, { id: event.stream.id, peerConnection }]);
-    }
-  };
-  
-  // Отправка запроса на создание комнаты и получение идентификатора комнаты
-  socket.emit('createRoom', (roomId) => {
-    setRoomId(roomId);
-  });
-};
+    };
+    getLocalStream();
+  }, []);
 
-getLocalMediaStream();
-}, []);
-
-// Обработка события приема кандидата и добавление его в соответствующий RTCPeerConnection
-useEffect(() => {
-if (socket) {
-socket.on('candidate', ({ senderId, candidate }) => {
-const senderConnection = peerConnections.find(connection => connection.id === senderId).peerConnection;
-senderConnection.addIceCandidate(new RTCIceCandidate(candidate));
-});
-}
-}, [socket, peerConnections]);
-
-// Обработка события подключения нового пользователя к комнате
-useEffect(() => {
-if (socket) {
-socket.on('userConnected', ({ userId }) => {
-// Создание нового RTCPeerConnection и отправка кандидатов
-const newPeerConnection = new RTCPeerConnection();
-peerConnections.forEach(({ peerConnection }) => {
-newPeerConnection.addIceCandidate(new RTCIceCandidate(peerConnection.localDescription));
-peerConnection.addIceCandidate(new RTCIceCandidate(newPeerConnection.localDescription));
-peerConnection.createOffer()
-.then((description) => {
-peerConnection.setLocalDescription(description);
-newPeerConnection.setRemoteDescription(description);
-newPeerConnection.createAnswer()
-.then((description) => {
-newPeerConnection.setLocalDescription(description);
-peerConnection.setRemoteDescription(description);
-});
-});
-});
-  // Добавление нового видеоэлемента для отображения удаленного потока медиа
-  remoteVideosRef.current = [...remoteVideosRef.current, newRemoteVideoRef];
-  setPeerConnections(prevState => [...prevState, { id: userId, peerConnection: newPeerConnection }]);
-});
-}
-}, [socket, peerConnections]);
-
-// Обработка события отключения пользователя от комнаты
-useEffect(() => {
-if (socket) {
-socket.on('userDisconnected', ({ userId }) => {
-// Удаление RTCPeerConnection и связанного видеоэлемента
-const removedConnection = peerConnections.find(connection => connection.id === userId);
-// removedConnection.peerConnection.close();
-// setPeerConnections(prevState => prevState.filter(connection => connection.id !== userId)
-
-      // Удаление связанного видеоэлемента
-      remoteVideosRef.current = remoteVideosRef.current.filter(ref => ref.id !== removedConnection.peerConnection.stream.id);
-      setPeerConnections(prevState => prevState.filter(connection => connection.id !== userId));
-    });
-  }
-}, [socket, peerConnections])
-// Отправка сообщения через WebSocket
-const sendMessage = (message) => {
-  if (socket) {
-  socket.emit('message', { roomId, message });
-  }
-  };
-  
-  const [messages,setMessages]=useState()
-  const [message,setMessage]=useState()
-  // Обработка события приема сообщения через WebSocket
   useEffect(() => {
-  if (socket) {
-  socket.on('message', ({ senderId, message }) => {
-  const senderName = peerConnections.find(connection => connection.id === senderId)?.name || 'Unknown';
-  const formattedMessage = {senderName}+':'+{message};
-  setMessages(prevState => [...prevState, formattedMessage]);
-  });
-  }
-  }, [socket, peerConnections]);
-  
-  return (
-  <div>
-  <video ref={localVideoRef} autoPlay muted />
-  {remoteVideosRef.current.map((ref) => (
-  <video key={ref.id} ref={ref} autoPlay />
-  ))}
-  <input type="text" value={message} onChange={(event) => setMessage(event.target.value)} />
-  <button onClick={() => sendMessage(message)}>Send</button>
-  {messages.map((message, index) => (
-  <div key={index}>{message}</div>
-  ))}
-  </div>
-  );
-  };
-  
-  export default Chat;
+    const createPeerConnection = (index) => {
+      const peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: "stun:stun.l.google.com:19302",
+          },
+        ],
+      });
+      setPeerConnections((prev) => [...prev, peer]);
+
+      peer.onicecandidate = (event) => {
+        if (event.candidate) {
+          const candidate = event.candidate;
+          // send candidate to other peers
+        }
+      };
+
+      peer.ontrack = (event) => {
+        const remoteVideo = remoteVideosRef.current[index];
+        if (remoteVideo) {
+          remoteVideo.srcObject = event.streams[0];
+        }
+      };
+
+      if (localStream) {
+        localStream.getTracks().forEach((track) => {
+          peer.addTrack(track, localStream);
+        });
+      }
+
+      return peer;
+    };
+
+    if (localStream) {
+      const socket = new WebSocket(`wss://rims.by/ws/videoconference/${roomId}/`);
+      socket.onopen = () => {
+        console.log("WebSocket connection established");
+        socket.send(JSON.stringify({ type: "join_room", roomId }));
+      };
+      socket.onmessage = (event) => {
+        const message = JSON.parse(event.data);
+        switch (message.type) {
+          case "joined_room":
+            console.log(`Joined room ${message.roomId}`);
+            break;
+          case "peer_joined":
+            console.log(`Peer ${message.peerId} joined the room`);
+            const peerConnection = createPeerConnection(remoteStreams.length);
+            peerConnection.createOffer().then((offer) => {
+              peerConnection.setLocalDescription(offer);
+              socket.send(JSON.stringify({
+                type: "offer",
+                peerId: message.peerId,
+                offer,
+              }));
+            });
+            setRemoteStreams((prev) => [...prev, null]);
+            break;
+          case "offer":
+            console.log(`Received offer from peer ${message.peerId}`);
+            const peerConnection2 = createPeerConnection(remoteStreams.length);
+            peerConnection2.setRemoteDescription(new RTCSessionDescription(message.offer));
+            peerConnection2.createAnswer().then((answer) => {
+              peerConnection2.setLocalDescription(answer);
+              socket.send(JSON.stringify({
+                type: "answer",
+                peerId: message.peerId,
+                answer,
+              }));
+            });
+            setRemoteStreams((prev) => [...prev, null]);
+            break;
+          case "answer":
+            console.log(`Received answer from peer ${message.peerId}`);
+            const peerConnection3 = peerConnections[message.peerId];
+            peerConnection3.setRemoteDescription(new RTCSessionDescription(message.answer));
+            break;
+          case "candidate":
+            console.log(`Received candidate from peer ${message.peerId}`);
+            const peerConnection4 = peerConnections[message.peerId];
+            peerConnection4.addIceCandidate(new RTCIceCandidate
+              ({
+                sdpMLineIndex: message.candidate.sdpMLineIndex,
+                candidate: message.candidate.candidate,
+                }));
+                break;
+                case "peer_left":
+                console.log(`Peer ${message.peerId} left the room`);
+                const peerConnection5 = peerConnections[message.peerId];
+                if (peerConnection5) {
+                peerConnection5.close();
+                setPeerConnections((prev) => {
+                prev.splice(message.peerId, 1);
+                return [...prev];
+                });
+                }
+                setRemoteStreams((prev) => {
+                prev.splice(message.peerId, 1);
+                return [...prev];
+                });
+                break;
+                default:
+                console.warn(`Unknown message type: ${message.type}`);
+                break;
+                }
+                };
+                return () => {
+                socket.close();
+                peerConnections.forEach((peer) => peer.close());
+                };
+                }
+                }, [localStream, roomId, peerConnections]);
+                
+                const handleJoinRoom = () => {
+                // connect to signaling server and join the specified room
+                };
+                
+                return (
+                <div>
+                <h1>WebRTC Video Conference</h1>
+                <div>
+                <video ref={localVideoRef} autoPlay muted />
+                {remoteStreams.map((stream, index) => (
+                <video key={index} ref={(video) => (remoteVideosRef.current[index] = video)} autoPlay />
+                ))}
+                </div>
+                <div>
+                <label htmlFor="room-id-input">Room ID:</label>
+                <input id="room-id-input" value={roomId} onChange={(e) => setRoomId(e.target.value)} />
+                <button onClick={handleJoinRoom}>Join Room</button>
+                </div>
+                </div>
+                );
+                };
+                
+                export default WebRTCVideoConference;
