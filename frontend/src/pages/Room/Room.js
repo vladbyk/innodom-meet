3cc9898c880cf4c55ca762875ac2f26af7ff56db
Katia <vai.katia03@mail.ru>
2023-03-27 14:54:20 +0300

    new-meet7

--- frontend/src/pages/Room/Room.js
+++ frontend/src/pages/Room/Room.js
@@ -2,11 +2,59 @@ import axios from "axios";
 import React, {useState, useEffect, useRef, useCallback} from "react";
 
 const Room = (data) => {
+    const config = {
+        iceServers: [
+          {
+            urls: "stun:stun.rims.by:5349",
+          },
+          {
+            "url": "turn:turn.rims.by:5349",
+            "username": "guest",
+            "credential": "somepassword"
+          }
+        ],
+      };
+
+    let localVideo = useRef()
+    let localStream;
+    let peerConnection=useRef()
+    const beReady = () => {
+        return navigator.mediaDevices.getUserMedia({
+         audio: true,
+         video: true,
+       }).then((stream)=>{
+       localStream = stream;
+       localVideo.current.srcObject = stream;
+       return createConnectionAndAddStream();
+     }).catch( (err)=>{
+       console.log(err);
+     })
+   };
+   const createConnectionAndAddStream = () => {
+    createPeerConnection();
+    peerConnection.current.addStream(localStream);
+    return true;
+  };
+  const createPeerConnection = () => {
+    try {
+      peerConnection.current = new RTCPeerConnection(config);
+    //   peerConnection.current.onicecandidate = handleIceCandidate;
+    //   peerConnection.current.onaddstream = handleRemoteStreamAdded;
+    //   peerConnection.current.onremovestream = handleRemoteStreamRemoved;
+      console.log("create rtc peer connection");
+      return;
+    } catch (e) {
+      console.log("faild peer ", e.message);
+      return;
+    }
+  };
+
     const connectRoom = () => {
         console.log('sok',data.data)
         const socket = new WebSocket(`wss://rims.by/ws/room/${data.data.group}/`);
         socket.onopen = () => {
             console.log("WebSocket connection established");
+            beReady()
             // socket.send(JSON.stringify({type: "join_room", roomId}));
         };
         // socket.onmessage = (event) => {
@@ -16,12 +64,14 @@ const Room = (data) => {
         //     socket.close();
         // };
     };
+
     useEffect(()=>{
         connectRoom()
     },[])
     return (
        <div>
            <h1>room</h1>
+           <video muted autoPlay ref={localVideo}></video>
        </div>
    );
 }
