a9a7b5a7b1397e88647a8142190bbb739f3f5ee5
vladbyk <byk_vlad@inbox.ru>
2023-03-27 13:32:15 +0300

    csrf trusted origins

--- backend/meet/views.py
+++ backend/meet/views.py
@@ -1,8 +1,2 @@
-from django.http import JsonResponse
 from .models import Room
 
-
-def room_list(request):
-    rooms = Room.objects.all()
-    data = [{'id': room.id, 'name': room.name} for room in rooms]
-    return JsonResponse({'rooms': data})
