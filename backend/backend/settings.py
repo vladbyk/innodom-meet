fe4ec55f9e293a74686239a8b1583a6d94003abb
vladbyk <byk_vlad@inbox.ru>
2023-03-27 12:07:08 +0300

    csrf trusted origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
+STATIC_ROOT = BASE_DIR / 'django_static'
 
 # For Local Development
 # STATICFILES_DIRS = [
