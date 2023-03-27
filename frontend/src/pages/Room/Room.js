ea44f0b78d7fe096a70ac851c2b871e7893e97cd
Katia <vai.katia03@mail.ru>
2023-03-27 14:39:21 +0300

    new-meet6

--- frontend/src/pages/Room/Room.js
+++ frontend/src/pages/Room/Room.js
@@ -3,8 +3,8 @@ import React, {useState, useEffect, useRef, useCallback} from "react";
 
 const Room = (data) => {
     const connectRoom = () => {
-        console.log('sok',data)
-        const socket = new WebSocket(`wss://rims.by/ws/room/${data.group}/`);
+        console.log('sok',data.data)
+        const socket = new WebSocket(`wss://rims.by/ws/room/${data.data.group}/`);
         socket.onopen = () => {
             console.log("WebSocket connection established");
             // socket.send(JSON.stringify({type: "join_room", roomId}));
