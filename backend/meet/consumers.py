c03172dadd3f701f96ee9c9e2dc670531c6967c0
vladbyk <byk_vlad@inbox.ru>
2023-04-03 18:35:13 +0300

    csrf trusted origins

--- backend/meet/consumers.py
+++ backend/meet/consumers.py
@@ -80,7 +80,7 @@ class VideoConferenceConsumer(AsyncWebsocketConsumer):
             )
         elif message['type'] == 'disconnect':
             user = Conference.objects.get(user__id=message['user'])
-            for user_conf in Conference.objects.filter(group=message['group']).exclude(user=user):
+            for user_conf in Conference.objects.filter(user__group=message['group']).exclude(user=user):
                 await channel_layer.send(
                     user_conf.channel_name, {
                         'type': 'getDisconnect',
