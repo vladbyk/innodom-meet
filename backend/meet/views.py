9b580963aef33d6ec736cfcd321a7aa45c8e6c24
vladbyk <byk_vlad@inbox.ru>
2023-05-11 17:15:31 +0300

    csrf trusted origins

--- backend/meet/views.py
+++ backend/meet/views.py
@@ -1,4 +1,5 @@
 import base64
+import os.path
 
 from rest_framework import status
 from rest_framework.decorators import permission_classes, api_view
@@ -20,36 +21,37 @@ def get_movie(request):
             group=Room.objects.get(group=request.data['group'])
         ).save()
     else:
-        with open('../videos/file.webm', 'wb') as f_vid:
+        path = os.path.dirname(__file__).replace('meet','videos')
+        with open(f'{path}/file.webm', 'wb') as f_vid:
             f_vid.write(base64.b64encode(movie[0].moveis))
-        with open('../videos/file1.webm', 'wb') as f_vid:
+        with open(f'{path}/file1.webm', 'wb') as f_vid:
             f_vid.write(base64.b64encode(request.data['blob']))
-        clip = VideoFileClip("../videos/file.webm")
+        clip = VideoFileClip(f"{path}/file.webm")
         clip1 = clip.subclip(0, 5)
-        clipx = VideoFileClip("../videos/file1.webm")
+        clipx = VideoFileClip(f"{path}/file1.webm")
         clip2 = clipx.subclip(0, 5)
         clips = [clip1, clip2]
         final = concatenate_videoclips(clips)
-        if os.path.isfile('../videos/file.webm'):
-            os.remove('../videos/file.webm')
-        if os.path.isfile('../videos/file1.webm'):
-            os.remove('../videos/file1.webm')
+        if os.path.isfile(f"{path}/file.webm"):
+            os.remove(f"{path}/file.webm")
+        if os.path.isfile(f"{path}/file1.webm"):
+            os.remove(f"{path}/file1.webm")
         if request.data['is_last']:
-            final.write_videofile(f'../videos/{movie[0].group}-{movie[0].date}.webm')
+            final.write_videofile(f"{path}/{movie[0].group}-{movie[0].date}.webm")
             y = yadisk.YaDisk(token="y0_AgAAAABVA64gAAnjtwAAAADi8QQ2wmDmWZ8PSC2W0S6RFne6SwNOxvA")
             y.mkdir("/videos")
             y.upload(f"../videos/{movie[0].group}-{movie[0].date}.webm", f"/videos/{movie[0].group}-{movie[0].date}.webm")
-            if os.path.isfile(f'../videos/{movie[0].group}-{movie[0].date}.webm'):
-                os.remove(f'../videos/{movie[0].group}-{movie[0].date}.webm')
+            if os.path.isfile(f"{path}/{movie[0].group}-{movie[0].date}.webm"):
+                os.remove(f"{path}/{movie[0].group}-{movie[0].date}.webm")
             movie[0].delete()
         else:
-            final.write_videofile(f'../videos/{movie[0].group}-{movie[0].date}.webm')
-            with open(f'../videos/{movie[0].group}-{movie[0].date}.webm', 'rb') as f_vid:
+            final.write_videofile(f"{path}/{movie[0].group}-{movie[0].date}.webm")
+            with open(f"{path}/{movie[0].group}-{movie[0].date}.webm", 'rb') as f_vid:
                 MoviesGenerate(
                     movies=base64.b64decode(f_vid.read()),
                     group=Room.objects.get(group=request.data['group'])
                 ).save()
-            if os.path.isfile(f'../videos/{movie[0].group}-{movie[0].date}.webm'):
-                os.remove(f'../videos/{movie[0].group}-{movie[0].date}.webm')
+            if os.path.isfile(f"{path}/{movie[0].group}-{movie[0].date}.webm"):
+                os.remove(f"{path}/{movie[0].group}-{movie[0].date}.webm")
             movie[0].delete()
     return Response(status=status.HTTP_200_OK)
