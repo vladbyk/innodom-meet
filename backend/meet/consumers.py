3df7ce10aa9471cceed4fe550be7378d917fd65a
vladbyk <byk_vlad@inbox.ru>
2023-07-18 16:33:33 +0300

    csrf trusted origins

--- backend/meet/consumers.py
+++ backend/meet/consumers.py
@@ -139,7 +139,7 @@ class VideoConferenceConsumer(AsyncWebsocketConsumer):
                 await channel_layer.send(
                     teacher.channel_name, {
                         'type': 'getCheckSharingOffer',
-                        'channel_name': message['channel_name']
+                        'channel_name': user.channel_name
                     }
                 )
         elif message['type'] == 'sharingAnswer':
