de1d84a8ff0a35a6ee2781b49bf55e147e934cc6
vladbyk <byk_vlad@inbox.ru>
2023-08-03 15:52:25 +0300

    csrf trusted origins

--- backend/meet/consumers.py
+++ backend/meet/consumers.py
@@ -203,8 +203,10 @@ class VideoConferenceConsumer(AsyncWebsocketConsumer):
                 )
         elif message['type'] == 'personalCameraMute':
             if message['user'][0] == 's':
+                print('!')
                 user = Conference.objects.get(channel_name=message['user'])
                 message['user'] = user.user.id
+            print(message['user'])
             for user_conf in Conference.objects.filter(user__group__group=message['group']):
                 await channel_layer.send(
                     user_conf.channel_name, {
