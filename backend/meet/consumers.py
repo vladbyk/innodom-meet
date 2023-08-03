cda25a2cd0287ca3751616a7400032871fca873d
vladbyk <byk_vlad@inbox.ru>
2023-08-03 15:58:49 +0300

    csrf trusted origins

--- backend/meet/consumers.py
+++ backend/meet/consumers.py
@@ -193,7 +193,7 @@ class VideoConferenceConsumer(AsyncWebsocketConsumer):
                     }
                 )
         elif message['type'] == 'personalMicrophoneMute':
-            if message['user'][0] == 's':
+            if str(message['user'])[0] == 's':
                 user = Conference.objects.get(channel_name=message['user'])
                 message['user'] = user.user.id
             for user_conf in Conference.objects.filter(user__group__group=message['group']):
@@ -205,7 +205,7 @@ class VideoConferenceConsumer(AsyncWebsocketConsumer):
                     }
                 )
         elif message['type'] == 'personalCameraMute':
-            if message['user'][0] == 's':
+            if str(message['user'])[0] == 's':
                 user = Conference.objects.get(channel_name=message['user'])
                 message['user'] = user.user.id
             for user_conf in Conference.objects.filter(user__group__group=message['group']):
